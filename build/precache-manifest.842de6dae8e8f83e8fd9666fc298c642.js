d3167eaedd0c9146aa31f761920f82dcd77a9db8
iambinhnpt <iambinhnpt@gmail.com>
2020-09-12 13:26:58 +0700

    make it better

---
+++ build/precache-manifest.842de6dae8e8f83e8fd9666fc298c642.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c77a6fc0ccce3813e36b767de4842330",
+    "url": "/movie-example/index.html"
+  },
+  {
+    "revision": "9dffc86a232521606928",
+    "url": "/movie-example/static/css/2.d5466ada.chunk.css"
+  },
+  {
+    "revision": "d00daef9e21d786e3875",
+    "url": "/movie-example/static/css/main.e7f8f978.chunk.css"
+  },
+  {
+    "revision": "9dffc86a232521606928",
+    "url": "/movie-example/static/js/2.a528f4c0.chunk.js"
+  },
+  {
+    "revision": "d00daef9e21d786e3875",
+    "url": "/movie-example/static/js/main.34952f25.chunk.js"
+  },
+  {
+    "revision": "d066edc0f2cf43be36a0",
+    "url": "/movie-example/static/js/runtime-main.b9c84a9f.js"
+  },
+  {
+    "revision": "12d095ee01d4c66afd2a1924eec3e5f5",
+    "url": "/movie-example/static/media/TMDBLogo.12d095ee.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/movie-example/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/movie-example/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/movie-example/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/movie-example/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/movie-example/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/movie-example/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/movie-example/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/movie-example/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "2f8150f9d2780aa084714418fd04acb2",
+    "url": "/movie-example/static/media/side-image.2f8150f9.jpeg"
+  }
+]);
